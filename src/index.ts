8ea75b543dc94cf5721ae7f9203351c1395fadef
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-14 15:30:09 +0300

    fix: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Maze from "./maze";
 import Snackbar from "./snackbar";
 
-const save_btn = document.getElementById("save-btn")!;
-const load_btn = document.getElementById("load-btn")!;
+const save_btn = document.getElementById("save")!;
+const load_btn = document.getElementById("load")!;
 
 const maze = new Maze(10, 10);
 maze.create();
