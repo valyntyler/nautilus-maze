474c506047bd21821315bc9431de61f994ef29c3
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-15 22:53:39 +0300

    refactor: update direction.ts

--- src/direction.ts
+++ src/direction.ts
@@ -1,8 +1,23 @@
-const enum Direction {
+enum Direction {
   Up,
   Down,
   Left,
   Right,
+}
+
+namespace Direction {
+  export function id(dir: Direction): string {
+    switch (dir) {
+      case Direction.Up:
+        return "up";
+      case Direction.Down:
+        return "down";
+      case Direction.Left:
+        return "left";
+      case Direction.Right:
+        return "right";
+    }
+  }
 }
 
 export default Direction;
