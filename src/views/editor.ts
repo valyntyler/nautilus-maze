aa1df24b05b9268d78fa7f70193462aaab32d4ea
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 01:56:48 +0300

    feat: update editor.ts

--- src/views/editor.ts
+++ src/views/editor.ts
@@ -5,10 +5,28 @@ export default class Editor {
   container: HTMLDivElement;
   toolbar: Toolbar;
 
-  set state(value: State) {}
+  get state(): State {
+    return new State();
+  }
+
+  set state(value: State) {
+    this.container.innerHTML = "";
+    this.container.style.gridTemplateColumns = `repeat(${this.state.cols}, 32px)`;
+    this.container.style.gridTemplateRows = `repeat(${this.state.rows}, 32px)`;
+
+    for (let i = 0; i < this.state.rows; i++) {
+      for (let j = 0; j < this.state.cols; j++) {
+        const cell = document.createElement("div");
+        cell.className = "maze-cell";
+
+        this.container.appendChild(cell);
+      }
+    }
+  }
 
   constructor(toolbar: Toolbar) {
     this.toolbar = toolbar;
     this.container = document.getElementById("maze")! as HTMLDivElement;
+    this.state = new State();
   }
 }
