a92cc50bf19f82c8505a5a166034cce20d03bf5c
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 23:19:16 +0300

    refactor: update save.ts

--- src/model/save.ts
+++ src/model/save.ts
@@ -1,18 +1,7 @@
-import Player from "./player";
+import Maze from "./maze";
+import Robot from "./robot/robot";
 
-export default class Save {
-  constructor(
-    readonly rows: number = 14,
-    readonly cols: number = 15,
-    readonly grid: number[][] = [],
-    readonly start: Player = new Player(),
-  ) {
-    if (grid.length === 0) {
-      grid = Array(rows)
-        .fill([])
-        .map(() => Array(cols).fill(0));
-    }
-
-    return { rows, cols, grid, start };
-  }
+export default interface Save {
+  robot: Robot;
+  maze: Maze;
 }
