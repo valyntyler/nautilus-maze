d84fbf3c56fe55e3e923370e84e583da727ceae2
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 18:48:57 +0300

    feat: create index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+const view_btn = document.getElementById("view")!;
+const edit_btn = document.getElementById("edit")!;
+
+view_btn.addEventListener("mousedown", () => {});
+
+edit_btn.addEventListener(
+  "mousedown",
+  () => (window.location.href = "./pages/editor.html"),
+);
