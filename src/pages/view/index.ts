bf8aae05b2eaee24b82b3d126a5aed9a0045107e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-17 00:05:00 +0300

    feat: working left command

--- src/pages/view/index.ts
+++ src/pages/view/index.ts
@@ -59,6 +59,30 @@ document.addEventListener("DOMContentLoaded", () => {
           }
           break;
 
+        case Command.Left:
+          while (true) {
+            await delay(200);
+
+            const move = Rotation.step(runner.robot.dir);
+
+            const new_x = runner.robot.x + move.x;
+            const new_y = runner.robot.y + move.y;
+
+            if (new_x >= runner.state.rows || new_x < 0) break;
+            if (new_y >= runner.state.rows || new_y < 0) break;
+
+            if (runner.cell(new_x, new_y).classList.contains("black")) break;
+            if (
+              !runner
+                .cell(runner.robot.x - 1, runner.robot.y)
+                .classList.contains("black")
+            )
+              break;
+
+            runner.robot = new Player(new_x, new_y, runner.robot.dir);
+          }
+          break;
+
         case Command.Turn:
           await delay(200);
           runner.robot = new Player(
