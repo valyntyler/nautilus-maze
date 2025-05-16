bf8b15a283d7e2d30636d04f1e9e64499b6895c5
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 22:09:39 +0300

    feat: command parse function

--- src/model/robot/command.ts
+++ src/model/robot/command.ts
@@ -6,4 +6,28 @@ enum Command {
   Turn,
 }
 
+namespace Command {
+  export function parse(s: string): Command | null {
+    switch (s.toLowerCase()) {
+      case "step":
+        return Command.Step;
+
+      case "back":
+        return Command.Back;
+
+      case "move":
+        return Command.Move;
+
+      case "left":
+        return Command.Left;
+
+      case "turn":
+        return Command.Turn;
+
+      default:
+        return null;
+    }
+  }
+}
+
 export default Command;
