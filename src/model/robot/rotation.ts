6440f90bd86e001d19ebb82c44a18f1a1aa183eb
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 22:43:05 +0300

    feat: rotation back function

--- src/model/robot/rotation.ts
+++ src/model/robot/rotation.ts
@@ -35,6 +35,34 @@ namespace Rotation {
         };
     }
   }
+
+  export function back(rotation: Rotation): Position {
+    switch (rotation) {
+      case Rotation.North:
+        return {
+          x: 0,
+          y: -1,
+        };
+
+      case Rotation.South:
+        return {
+          x: 0,
+          y: 1,
+        };
+
+      case Rotation.East:
+        return {
+          x: 1,
+          y: 0,
+        };
+
+      case Rotation.West:
+        return {
+          x: -1,
+          y: 0,
+        };
+    }
+  }
 }
 
 export default Rotation;
