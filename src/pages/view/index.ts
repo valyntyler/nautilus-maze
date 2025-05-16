7b437698bf0e334233b871eaed28e6fbb0ee9adc
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 20:22:57 +0300

    feat: update view/index.ts

--- src/pages/view/index.ts
+++ src/pages/view/index.ts
@@ -1,9 +1,12 @@
 import Maze from "../../model/maze";
 import Runner from "../../components/runner";
+import Snackbar from "../../components/snackbar";
 
 document.addEventListener("DOMContentLoaded", () => {
   const edit_btn = document.getElementById("edit")!;
   const back_btn = document.getElementById("back")!;
+
+  const import_btn = document.getElementById("import")!;
 
   const runner = new Runner();
 
@@ -21,4 +24,27 @@ document.addEventListener("DOMContentLoaded", () => {
     "click",
     () => (window.location.href = "../../index.html"),
   );
+
+  import_btn.addEventListener("mousedown", () => {
+    const input = document.createElement("input");
+    input.type = "file";
+
+    input.multiple = false;
+    input.accept = ".json";
+
+    input.onchange = (event) => {
+      const file = (event.target as any).files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const target = e.target as any;
+        runner.state = JSON.parse(target.result);
+
+        Snackbar.show("Import successful!");
+      };
+      reader.readAsText(file);
+    };
+
+    input.click();
+  });
 });
