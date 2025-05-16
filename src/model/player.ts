62fc52e1c4e843ca580471de58e79c618e0267d2
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 23:12:58 +0300

    chore: remove player.ts

--- src/model/player.ts
+++
@@ -1,9 +0,0 @@
-import Direction from "./direction";
-
-export default class Player {
-  constructor(
-    public x: number = 0,
-    public y: number = 0,
-    public dir: Direction = Direction.Up,
-  ) {}
-}
