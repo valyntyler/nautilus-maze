6f3f59d90a4719a1d5bb1777179b28e3b7362676
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-16 22:42:11 +0300

    feat: rotation move function

--- src/model/robot/rotation.ts
+++ src/model/robot/rotation.ts
@@ -1,8 +1,40 @@
+import Position from "./position";
+
 enum Rotation {
   North,
   South,
   East,
   West,
+}
+
+namespace Rotation {
+  export function move(rotation: Rotation): Position {
+    switch (rotation) {
+      case Rotation.North:
+        return {
+          x: 0,
+          y: 1,
+        };
+
+      case Rotation.South:
+        return {
+          x: 0,
+          y: -1,
+        };
+
+      case Rotation.East:
+        return {
+          x: -1,
+          y: 0,
+        };
+
+      case Rotation.West:
+        return {
+          x: 1,
+          y: 0,
+        };
+    }
+  }
 }
 
 export default Rotation;
