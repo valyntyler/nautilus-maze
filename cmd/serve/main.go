7a257e93cb94f28550ed006faf65f7f0d530e025
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-17 18:54:05 +0300

    fix: static servic dist/

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	port := 8080
 
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 
 	fmt.Printf("Server open at http://localhost:%d\n", port)
-
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatal(err)
