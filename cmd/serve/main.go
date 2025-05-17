1a6c5d38a082ec2a4ca6dfde432a45cb9de2c499
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-17 19:36:29 +0300

    refactor: rename src/public

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	port := 8080
 
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/", http.FileServer(http.Dir("./src/public")))
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 
 	fmt.Printf("Server open at http://localhost:%d\n", port)
