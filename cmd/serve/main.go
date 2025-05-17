7ef5a422160c93b3981d2cb73f807873b5711487
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-17 17:21:30 +0300

    feat: go serve script

---
+++ cmd/serve/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := 8080
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	fmt.Printf("Server open at http://localhost:%d\n", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
