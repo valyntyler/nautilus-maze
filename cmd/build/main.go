a20db6267f0c662f9ab32231d59413030871b3e7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-17 16:39:49 +0300

    feat: go build script

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/evanw/esbuild/pkg/api"
+)
 
 func main() {
-	fmt.Println("Hello from Go!")
+	result := api.Build(api.BuildOptions{
+		EntryPoints: []string{
+			"src/pages/index.ts",
+			"src/pages/view/index.ts",
+			"src/pages/edit/index.ts",
+		},
+		Bundle: true,
+		Outdir: "dist",
+		Write:  true,
+	})
+
+	for _, msg := range result.Warnings {
+		fmt.Print("WARNING: ")
+		fmt.Println(msg.Text)
+	}
+
+	for _, msg := range result.Errors {
+		fmt.Print("ERROR: ")
+		fmt.Println(msg.Text)
+	}
+
+	if len(result.Errors) != 0 {
+		os.Exit(1)
+	} else {
+		fmt.Println("Build successful!")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module valyntyler.com/nautilus-maze
 
 go 1.24.1
+
+require github.com/evanw/esbuild v0.25.4
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
