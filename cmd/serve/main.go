043b0b2b5d87d87577c7681828f51f70c55823ba
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-17 18:58:42 +0300

    fix: static serving styles/

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -11,6 +11,7 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("./styles"))))
 
 	fmt.Printf("Server open at http://localhost:%d\n", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
