b306173fc2a45f34ccb2c9a4f6024c37b905e75e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-18 18:46:12 +0300

    feat: displaying tools

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,9 @@
 import "../styles/style.css";
 import MazeComponent from "./common/maze_component";
 import MazeData from "./common/maze_data";
+import Tools from "./common/tools";
 
 document.addEventListener("DOMContentLoaded", () => {
   const maze = new MazeComponent(new MazeData(14, 15));
+  const tools = new Tools();
 });
