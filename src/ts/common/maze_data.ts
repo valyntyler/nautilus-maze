748ad779200f73d54393a23bac7fa71899d03440
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-18 17:17:24 +0300

    feat: update maze_data.ts

--- src/ts/common/maze_data.ts
+++ src/ts/common/maze_data.ts
@@ -3,6 +3,14 @@ import Grid from "../data/grid";
 
 class MazeData implements Grid {
   data: Cell[][];
+
+  get rows() {
+    return this.data.length;
+  }
+
+  get cols() {
+    return this.data[0].length;
+  }
 }
 
 export default MazeData;
