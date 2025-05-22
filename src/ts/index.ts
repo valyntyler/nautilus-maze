43e261f1cb8e811b4e21aed7ef6f8521633673e2
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-22 18:45:00 +0300

    feat: drawing and erasing

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,27 @@
 import "../styles/style.css";
 import MazeComponent from "./common/maze_component";
 import MazeData from "./common/maze_data";
+import Tool from "./common/tool";
 import Tools from "./common/tools";
 
 document.addEventListener("DOMContentLoaded", () => {
   const maze = new MazeComponent(new MazeData(14, 15));
   const tools = new Tools();
+
+  maze.oncellevent = (cell, event, state) => {
+    switch (tools.selected) {
+      case Tool.Pencil: {
+        if (state.left) {
+          cell.classList.add("black");
+        }
+        break;
+      }
+      case Tool.Eraser: {
+        if (state.left) {
+          cell.classList.remove("black");
+        }
+        break;
+      }
+    }
+  };
 });
