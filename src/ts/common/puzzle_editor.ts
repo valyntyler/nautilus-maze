09af25acbf9c0fceb79f062f01efc487539ef904
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-22 23:15:29 +0300

    refactor: update puzzle_editor.ts

--- src/ts/common/puzzle_editor.ts
+++ src/ts/common/puzzle_editor.ts
@@ -1,4 +1,6 @@
 import Rotation from "../data/rotation";
+import MouseEvent from "./mouse_event";
+import MouseState from "./mouse_state";
 import Puzzle from "./puzzle";
 import Tool from "./tool";
 import Tools from "./tools";
@@ -10,36 +12,41 @@ export default class PuzzleEditor extends Puzzle {
     super();
 
     this.tools = new Tools();
+    this.oncellevent = this.handleCellEvent;
+  }
 
-    this.oncellevent = (cell, event, state) => {
-      switch (this.tools.selected) {
-        case Tool.Pencil: {
-          if (state.left && cell.children.length === 0) {
-            cell.classList.add("black");
-          }
-          break;
+  private handleCellEvent(
+    cell: HTMLDivElement,
+    event: MouseEvent,
+    state: MouseState,
+  ) {
+    switch (this.tools.selected) {
+      case Tool.Pencil: {
+        if (state.left && cell.children.length === 0) {
+          cell.classList.add("black");
         }
-        case Tool.Eraser: {
-          if (state.left) {
-            cell.classList.remove("black");
-          }
-          break;
-        }
-        case Tool.Finger: {
-          const length = cell.children.length;
-          if (!cell.classList.contains("black")) {
-            if (state.left && length === 0) {
-              this.place(cell);
-            } else if (state.right && length !== 0) {
-              const id = cell.children[0].className;
-              const r = Rotation.parse(id)!;
-
-              this.place(cell, Rotation.turn(r));
-            }
-          }
-          break;
-        }
+        break;
       }
-    };
+      case Tool.Eraser: {
+        if (state.left) {
+          cell.classList.remove("black");
+        }
+        break;
+      }
+      case Tool.Finger: {
+        const length = cell.children.length;
+        if (!cell.classList.contains("black")) {
+          if (state.left && length === 0) {
+            this.place(cell);
+          } else if (state.right && length !== 0) {
+            const id = cell.children[0].className;
+            const r = Rotation.parse(id)!;
+
+            this.place(cell, Rotation.turn(r));
+          }
+        }
+        break;
+      }
+    }
   }
 }
