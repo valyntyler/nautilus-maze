0033b21d1d5b2d6ba98bc481c31e86592845ca37
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-22 23:18:29 +0300

    refactor: update puzzle_editor.ts

--- src/ts/common/puzzle_editor.ts
+++ src/ts/common/puzzle_editor.ts
@@ -22,30 +22,54 @@ export default class PuzzleEditor extends Puzzle {
   ) {
     switch (this.tools.selected) {
       case Tool.Pencil: {
-        if (state.left && cell.children.length === 0) {
-          cell.classList.add("black");
-        }
+        this.usePencil(cell, event, state);
         break;
       }
       case Tool.Eraser: {
-        if (state.left) {
-          cell.classList.remove("black");
-        }
+        this.useEraser(cell, event, state);
         break;
       }
       case Tool.Finger: {
-        const length = cell.children.length;
-        if (!cell.classList.contains("black")) {
-          if (state.left && length === 0) {
-            this.place(cell);
-          } else if (state.right && length !== 0) {
-            const id = cell.children[0].className;
-            const r = Rotation.parse(id)!;
-
-            this.place(cell, Rotation.turn(r));
-          }
-        }
+        this.useFinger(cell, event, state);
         break;
+      }
+    }
+  }
+
+  private usePencil(
+    cell: HTMLDivElement,
+    event: MouseEvent,
+    state: MouseState,
+  ) {
+    if (state.left && cell.children.length === 0) {
+      cell.classList.add("black");
+    }
+  }
+
+  private useEraser(
+    cell: HTMLDivElement,
+    event: MouseEvent,
+    state: MouseState,
+  ) {
+    if (state.left) {
+      cell.classList.remove("black");
+    }
+  }
+
+  private useFinger(
+    cell: HTMLDivElement,
+    event: MouseEvent,
+    state: MouseState,
+  ) {
+    const length = cell.children.length;
+    if (!cell.classList.contains("black")) {
+      if (state.left && length === 0) {
+        this.place(cell);
+      } else if (state.right && length !== 0) {
+        const id = cell.children[0].className;
+        const r = Rotation.parse(id)!;
+
+        this.place(cell, Rotation.turn(r));
       }
     }
   }
