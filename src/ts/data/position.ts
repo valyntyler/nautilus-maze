34f3e4eedbb7ca75a42b102556cd06f4a346b8c7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-22 19:30:46 +0300

    feat: update position.ts

--- src/ts/data/position.ts
+++ src/ts/data/position.ts
@@ -3,4 +3,10 @@ interface Position {
   y: number;
 }
 
+namespace Position {
+  export function create(x: number = 0, y: number = 0): Position {
+    return { x, y };
+  }
+}
+
 export default Position;
