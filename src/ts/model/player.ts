7620ebf8ac9bd156ff5b9cbd49e16209f5a16ec2
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-22 19:37:39 +0300

    chore: remove player.ts

--- src/ts/model/player.ts
+++
@@ -1,9 +0,0 @@
-import Rotation from "./robot/rotation";
-
-export default class Player {
-  constructor(
-    public x: number = 0,
-    public y: number = 0,
-    public dir: Rotation = Rotation.Up,
-  ) {}
-}
