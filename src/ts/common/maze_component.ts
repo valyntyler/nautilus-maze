c3a5bc0ab24dbd704504b687685c08faead629cd
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-22 18:30:20 +0300

    feat: update maze_component.ts

--- src/ts/common/maze_component.ts
+++ src/ts/common/maze_component.ts
@@ -1,8 +1,16 @@
 import MazeData from "./maze_data";
+import MouseEvent from "./mouse_event";
+import MouseState from "./mouse_state";
 import Robot from "./robot";
 
 export default class MazeComponent {
   private container: HTMLDivElement;
+
+  oncellevent: (
+    cell: HTMLDivElement,
+    event: MouseEvent,
+    state: MouseState,
+  ) => void;
 
   constructor(maze = new MazeData(), robot = new Robot()) {
     this.container = document.getElementById("maze") as HTMLDivElement;
@@ -13,6 +21,16 @@ export default class MazeComponent {
     for (let i = 0; i < maze.rows; i++) {
       for (let j = 0; j < maze.cols; j++) {
         const cell = document.createElement("div");
+
+        cell.addEventListener("mousedown", (e) =>
+          this.oncellevent(cell, MouseEvent.Down, MouseState.parse(e.buttons)),
+        );
+        cell.addEventListener("mouseenter", (e) =>
+          this.oncellevent(cell, MouseEvent.Enter, MouseState.parse(e.buttons)),
+        );
+
+        cell.addEventListener("contextmenu", (e) => e.preventDefault());
+
         this.container.appendChild(cell);
       }
     }
