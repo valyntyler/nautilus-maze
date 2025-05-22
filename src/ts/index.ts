91810a032f7e611ce596d4f711b2236909cfadda
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-22 19:39:45 +0300

    fix: update index.ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,10 +2,9 @@ import "../styles/style.css";
 import Puzzle from "./common/puzzle";
 import Tool from "./common/tool";
 import Tools from "./common/tools";
-import Grid from "./data/grid";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const puzzle = new Puzzle(Grid.create());
+  const puzzle = new Puzzle();
   const tools = new Tools();
 
   puzzle.oncellevent = (cell, event, state) => {
@@ -19,6 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
       case Tool.Eraser: {
         if (state.left) {
           cell.classList.remove("black");
+        }
+        break;
+      }
+      case Tool.Finger: {
+        if (!cell.classList.contains("black")) {
         }
         break;
       }
