b81874a1ad2b50b071e827c1c074f64ea8613459
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-23 01:21:23 +0300

    feat: update puzzle_runner.ts

--- src/ts/common/puzzle_runner.ts
+++ src/ts/common/puzzle_runner.ts
@@ -1,3 +1,24 @@
 import Puzzle from "./puzzle";
 
-export default class PuzzleRunner extends Puzzle {}
+export default class PuzzleRunner extends Puzzle {
+  constructor() {
+    super();
+
+    const playback = document.createElement("div");
+    const img = document.createElement("img");
+    const bar = document.getElementById("bar")!;
+
+    playback.id = "playback";
+    playback.className = "playback-container";
+
+    img.src = "./assets/bx-edit-alt.svg";
+    img.id = "edit";
+    img.onclick = () => {
+      console.log("edit");
+    };
+
+    bar.innerHTML = "";
+    bar.appendChild(img);
+    bar.appendChild(playback);
+  }
+}
