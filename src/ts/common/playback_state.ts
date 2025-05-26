c96e71609a9ecdec9989861c69ec6b70687357a7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-27 02:09:29 +0300

    feat: update playback_state.ts

--- src/ts/common/playback_state.ts
+++ src/ts/common/playback_state.ts
@@ -1,7 +1,25 @@
+import PlaybackButton from "./playback_button";
+
 enum PlaybackState {
   Ready,
   Playing,
   Ended,
+}
+
+namespace PlaybackState {
+  export function controls(state: PlaybackState): Array<PlaybackButton> {
+    switch (state) {
+      case PlaybackState.Ready: {
+        return [PlaybackButton.Prev, PlaybackButton.Next, PlaybackButton.Play];
+      }
+      case PlaybackState.Playing: {
+        return [];
+      }
+      case PlaybackState.Ended: {
+        return [];
+      }
+    }
+  }
 }
 
 export default PlaybackState;
