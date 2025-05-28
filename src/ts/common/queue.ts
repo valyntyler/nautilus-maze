0641d7073b784116a1679b7814ecec3ad565b9ee
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-28 18:09:19 +0300

    refactor: update queue.ts

--- src/ts/common/queue.ts
+++ src/ts/common/queue.ts
@@ -2,14 +2,39 @@ import Command from "../data/command";
 
 export default class Queue {
   private container: HTMLTextAreaElement;
+  private _index = 0;
 
-  get commands(): Array<Command> {
+  private get queue(): Array<Command> {
     const value = this.container.value;
     const queue = value.trim().split(/\s+/);
 
     return queue
       .map((string) => Command.parse(string))
       .filter((command) => command !== null);
+  }
+
+  get index(): number {
+    return this._index;
+  }
+
+  get next(): Command | null {
+    if (this.index >= this.queue.length) {
+      return null;
+    }
+
+    return this.queue[this.index++];
+  }
+
+  get prev(): Command | null {
+    if (this.index <= 0) {
+      return null;
+    }
+
+    return this.queue[this.index--];
+  }
+
+  private set index(value: number) {
+    this._index = value;
   }
 
   constructor() {
