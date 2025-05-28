4a93391fff14bf963a4285f29cdb3dc417df6730
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-28 20:31:50 +0300

    chore: update queue.ts

--- src/ts/common/queue.ts
+++ src/ts/common/queue.ts
@@ -2,35 +2,14 @@ import Command from "../data/command";
 
 export default class Queue {
   private container: HTMLTextAreaElement;
-  private _index = 0;
 
-  private get array(): Array<Command> {
+  get commands(): Array<Command> {
     const value = this.container.value;
     const queue = value.trim().split(/\s+/);
 
     return queue
       .map((string) => Command.parse(string))
       .filter((command) => command !== null);
-  }
-
-  get index(): number {
-    return this._index;
-  }
-
-  get current(): Command {
-    return this.array[this.index];
-  }
-
-  get rest(): Array<Command> {
-    let v: Array<Command> = [];
-    for (let k = this.index; k < this.array.length; k++) {
-      v.push(this.array[k]);
-    }
-    return v;
-  }
-
-  set index(value: number) {
-    this._index = value;
   }
 
   constructor() {
