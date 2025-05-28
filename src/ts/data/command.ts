5ee9cdaa1b2a16153139e1f5d66725bc1c670066
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-28 20:58:25 +0300

    refactor: update command.ts

--- src/ts/data/command.ts
+++ src/ts/data/command.ts
@@ -1,3 +1,6 @@
+import Grid from "./grid";
+import Transform from "./transform";
+
 enum Command {
   Step,
   Back,
@@ -28,6 +31,51 @@ namespace Command {
         return null;
       }
     }
+  }
+
+  export function run(cmd: Command, robot: Transform, maze: Grid): Transform {
+    switch (cmd) {
+      case Command.Step: {
+        const result = Transform.step(robot);
+        if (Grid.contains(result.position, maze)) {
+          return result;
+        }
+        return robot;
+      }
+      case Command.Back: {
+        const result = Transform.back(robot);
+        if (Grid.contains(result.position, maze)) {
+          return result;
+        }
+        return robot;
+      }
+      case Command.Move: {
+        // TODO:
+        return robot;
+      }
+      case Command.Left: {
+        // TODO:
+        return robot;
+      }
+      case Command.Turn: {
+        return Transform.turn(robot);
+      }
+    }
+  }
+
+  export function bake(
+    commands: Array<Command>,
+    robot: Transform,
+    maze: Grid,
+  ): Array<Transform> {
+    const out: Array<Transform> = [robot];
+
+    for (const cmd of commands) {
+      robot = Command.run(cmd, robot, maze);
+      out.push(robot);
+    }
+
+    return out;
   }
 }
 
