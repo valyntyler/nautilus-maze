2c796448c2a34704704bd7e9743d424f7e8d205e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 18:00:06 +0300

    feat: jank asf infinite rotation

--- src/ts/common/puzzle_editor.ts
+++ src/ts/common/puzzle_editor.ts
@@ -89,6 +89,20 @@ export default class PuzzleEditor extends Puzzle {
         x === this.robot.position.x &&
         y === this.robot.position.y
       ) {
+        if (this.robot.rotation === Rotation.Right) {
+          const rotations = parseInt(
+            getComputedStyle(document.documentElement).getPropertyValue(
+              "--rotations",
+            ),
+            10,
+          );
+
+          document.documentElement.style.setProperty(
+            "--rotations",
+            `${rotations + 1}`,
+          );
+        }
+
         this.robot.rotation = Rotation.turn(this.robot.rotation);
         this.robot.save();
       }
