8fdad945136d834bdd41a11fa5ef606d54f4b193
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 22:45:14 +0300

    refactor: update playback_event.ts

--- src/ts/common/playback/data/playback_event.ts
+++ src/ts/common/playback/data/playback_event.ts
@@ -8,13 +8,21 @@ type PlaybackEvent =
   | PlaybackEvent.Toggle;
 
 namespace PlaybackEvent {
-  export type Prev = { name: "prev" };
-  export type Next = { name: "next" };
-  export type Play = { name: "play" };
-  export type Stop = { name: "stop" };
-  export type Pause = { name: "pause" };
-  export type Resume = { name: "resume" };
-  export type Toggle = { name: "toggle" };
+  export const Prev = { name: "prev" } as const;
+  export const Next = { name: "next" } as const;
+  export const Play = { name: "play" } as const;
+  export const Stop = { name: "stop" } as const;
+  export const Pause = { name: "pause" } as const;
+  export const Resume = { name: "resume" } as const;
+  export const Toggle = { name: "toggle" } as const;
+
+  export type Prev = typeof Prev;
+  export type Next = typeof Next;
+  export type Play = typeof Play;
+  export type Stop = typeof Stop;
+  export type Pause = typeof Pause;
+  export type Resume = typeof Resume;
+  export type Toggle = typeof Toggle;
 }
 
 export default PlaybackEvent;
