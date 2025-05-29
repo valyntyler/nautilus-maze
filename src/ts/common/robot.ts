114b995d83900246dabb7288943b52871255ef5a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 15:57:40 +0300

    feat: update robot.ts

--- src/ts/common/robot.ts
+++ src/ts/common/robot.ts
@@ -1,20 +1,37 @@
 import Rotation from "../data/rotation";
+import Transform from "../data/transform";
 
 export default class Robot {
-  container: HTMLDivElement;
+  private container: HTMLDivElement;
+  private element: HTMLImageElement;
+
+  private _transform: Transform;
+
+  get transform(): Transform {
+    return this._transform;
+  }
+
+  set transform(value: Transform) {
+    this._transform = value;
+    this.element.dataset.rotation = Rotation.id(value.rotation);
+
+    const x = { property: "--robot-x", value: `${value.position.x}` };
+    const y = { property: "--robot-y", value: `${value.position.y}` };
+
+    document.documentElement.style.setProperty(x.property, x.value);
+    document.documentElement.style.setProperty(y.property, y.value);
+  }
 
   constructor() {
     this.container = document.getElementById("maze") as HTMLDivElement;
+    this.element = document.createElement("img");
 
-    const robot = document.createElement("img");
-    const rotation = Rotation.id(Rotation.Left);
+    this.element.id = "robot";
+    this.element.className = "robot";
+    this.element.src = "./assets/bx-caret-up.svg";
+    this.element.draggable = false;
+    this.element.dataset.rotation = Rotation.id(Rotation.Left);
 
-    robot.id = "robot";
-    robot.className = "robot";
-    robot.src = "./assets/bx-caret-up.svg";
-    robot.draggable = false;
-    robot.dataset.rotation = rotation;
-
-    this.container.appendChild(robot);
+    this.container.appendChild(this.element);
   }
 }
