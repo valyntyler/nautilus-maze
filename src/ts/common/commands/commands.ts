72683f6ab31b7f98b8d0a438fbcdec8a70e9dca5
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-30 02:05:05 +0300

    feat: populating commands field

--- src/ts/common/commands/commands.ts
+++ src/ts/common/commands/commands.ts
@@ -3,11 +3,12 @@ import Grid from "../../data/grid";
 import Transform from "../../data/transform";
 
 export default class Commands {
-  private container: HTMLTextAreaElement;
+  private container: HTMLDivElement;
   public steps: Array<Transform>;
 
   constructor(t: Transform, grid: Grid) {
-    // this.container = document.getElementById("commands") as HTMLTextAreaElement;
+    this.container = document.getElementById("commands") as HTMLDivElement;
+
     // this.steps = [
     //   t,
     //   ...this.container.value
@@ -23,10 +24,18 @@ export default class Commands {
     //
     // console.log(this.steps);
 
-    const form = document.getElementById("command-prompt")!;
+    const form = document.getElementById("command-prompt") as HTMLFormElement;
+    const input = form.elements.namedItem("command") as HTMLInputElement;
+
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-      console.log("YAY");
+
+      const div = document.createElement("div");
+      div.className = "command";
+      div.textContent = input.value;
+      this.container.appendChild(div);
+
+      input.value = "";
     });
   }
 }
