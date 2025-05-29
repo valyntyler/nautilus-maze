905214f4030bfa055a6ee067b2541a56e048eddb
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 22:34:12 +0300

    refactor: update playback_event.ts

--- src/ts/common/playback/data/playback_event.ts
+++ src/ts/common/playback/data/playback_event.ts
@@ -1,11 +1,20 @@
-enum PlaybackEvent {
-  Prev,
-  Next,
-  Play,
-  Stop,
-  Pause,
-  Resume,
-  Toggle,
+type PlaybackEvent =
+  | PlaybackEvent.Prev
+  | PlaybackEvent.Next
+  | PlaybackEvent.Play
+  | PlaybackEvent.Stop
+  | PlaybackEvent.Pause
+  | PlaybackEvent.Resume
+  | PlaybackEvent.Toggle;
+
+namespace PlaybackEvent {
+  export type Prev = { name: "prev" };
+  export type Next = { name: "next" };
+  export type Play = { name: "play" };
+  export type Stop = { name: "stop" };
+  export type Pause = { name: "pause" };
+  export type Resume = { name: "resume" };
+  export type Toggle = { name: "toggle" };
 }
 
 export default PlaybackEvent;
