56efa90d1d6de00aea43a54a6c4743cdd05c487f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 22:12:52 +0300

    refactor: update playback_button.ts

--- src/ts/common/playback/playback_button.ts
+++ src/ts/common/playback/playback_button.ts
@@ -1,58 +1,18 @@
-enum PlaybackButton {
-  Prev,
-  Next,
-  Play,
-  Stop,
-  Pause,
-  Reset,
-}
+type PlaybackButton =
+  | PlaybackButton.Prev
+  | PlaybackButton.Next
+  | PlaybackButton.Play
+  | PlaybackButton.Stop
+  | PlaybackButton.Pause
+  | PlaybackButton.Reset;
 
 namespace PlaybackButton {
-  export function name(btn: PlaybackButton): string {
-    switch (btn) {
-      case PlaybackButton.Prev: {
-        return "prev";
-      }
-      case PlaybackButton.Next: {
-        return "next";
-      }
-      case PlaybackButton.Play: {
-        return "play";
-      }
-      case PlaybackButton.Stop: {
-        return "stop";
-      }
-      case PlaybackButton.Pause: {
-        return "pause";
-      }
-      case PlaybackButton.Reset: {
-        return "reset";
-      }
-    }
-  }
-
-  export function source(btn: PlaybackButton): string {
-    switch (btn) {
-      case PlaybackButton.Prev: {
-        return "./assets/bx-skip-previous.svg";
-      }
-      case PlaybackButton.Next: {
-        return "./assets/bx-skip-next.svg";
-      }
-      case PlaybackButton.Play: {
-        return "./assets/bx-play.svg";
-      }
-      case PlaybackButton.Stop: {
-        return "./assets/bx-stop.svg";
-      }
-      case PlaybackButton.Pause: {
-        return "./assets/bx-pause.svg";
-      }
-      case PlaybackButton.Reset: {
-        return "./assets/bx-rotate-ccw.svg";
-      }
-    }
-  }
+  export type Prev = { name: "prev"; source: "./assets/bx-skip-previous.svg" };
+  export type Next = { name: "next"; source: "./assets/bx-skip-next.svg" };
+  export type Play = { name: "play"; source: "./assets/bx-play.svg" };
+  export type Stop = { name: "stop"; source: "./assets/bx-stop.svg" };
+  export type Pause = { name: "pause"; source: "./assets/bx-pause.svg" };
+  export type Reset = { name: "reset"; source: "./assets/bx-rotate-ccw.svg" };
 }
 
 export default PlaybackButton;
