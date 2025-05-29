c175fb838bd01dbbc9b40845db81cdc0f0af66c8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 23:20:09 +0300

    fix: update playback_button.ts

--- src/ts/common/playback/playback_button.ts
+++ src/ts/common/playback/playback_button.ts
@@ -9,41 +9,48 @@ type PlaybackButton =
   | PlaybackButton.Reset;
 
 namespace PlaybackButton {
-  export type Prev = {
-    name: "prev";
-    source: "./assets/bx-skip-previous.svg";
-    event: PlaybackEvent.Prev;
-  };
+  export const Prev = {
+    name: "prev",
+    source: "./assets/bx-skip-previous.svg",
+    event: PlaybackEvent.Prev,
+  } as const;
 
-  export type Next = {
-    name: "next";
-    source: "./assets/bx-skip-next.svg";
-    event: PlaybackEvent.Next;
-  };
+  export const Next = {
+    name: "next",
+    source: "./assets/bx-skip-next.svg",
+    event: PlaybackEvent.Next,
+  } as const;
 
-  export type Play = {
-    name: "play";
-    source: "./assets/bx-play.svg";
-    event: PlaybackEvent.Play;
-  };
+  export const Play = {
+    name: "play",
+    source: "./assets/bx-play.svg",
+    event: PlaybackEvent.Play,
+  } as const;
 
-  export type Stop = {
-    name: "stop";
-    source: "./assets/bx-stop.svg";
-    event: PlaybackEvent.Stop;
-  };
+  export const Stop = {
+    name: "stop",
+    source: "./assets/bx-stop.svg",
+    event: PlaybackEvent.Stop,
+  } as const;
 
-  export type Pause = {
-    name: "pause";
-    source: "./assets/bx-pause.svg";
-    event: PlaybackEvent.Pause;
-  };
+  export const Pause = {
+    name: "pause",
+    source: "./assets/bx-pause.svg",
+    event: PlaybackEvent.Pause,
+  } as const;
 
-  export type Reset = {
-    name: "reset";
-    source: "./assets/bx-rotate-ccw.svg";
-    event: PlaybackEvent.Stop;
-  };
+  export const Reset = {
+    name: "reset",
+    source: "./assets/bx-rotate-ccw.svg",
+    event: PlaybackEvent.Stop,
+  } as const;
+
+  export type Prev = typeof Prev;
+  export type Next = typeof Next;
+  export type Play = typeof Play;
+  export type Stop = typeof Stop;
+  export type Pause = typeof Pause;
+  export type Reset = typeof Reset;
 }
 
 export default PlaybackButton;
