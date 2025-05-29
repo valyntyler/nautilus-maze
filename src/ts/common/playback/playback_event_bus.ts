7ffbe0da6d8f59fb0725a5b242a615bd67b19c21
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 21:58:31 +0300

    feat: create event_bus.ts

---
+++ src/ts/common/playback/playback_event_bus.ts
@@ -0,0 +1,29 @@
+import PlaybackEvent from "./playback_event";
+
+export default class PlaybackEventBus {
+  private onevent?: (e: PlaybackEvent) => void;
+
+  constructor() {
+    document.addEventListener("keydown", (e) => {
+      if (!this.onevent) return;
+      switch (e.key) {
+        case "ArrowLeft": {
+          this.onevent(PlaybackEvent.Prev);
+          break;
+        }
+        case "ArrowRight": {
+          this.onevent(PlaybackEvent.Next);
+          break;
+        }
+        case "c": {
+          if (e.ctrlKey) this.onevent(PlaybackEvent.Stop);
+          break;
+        }
+        case "Space": {
+          this.onevent(PlaybackEvent.Toggle);
+          break;
+        }
+      }
+    });
+  }
+}
