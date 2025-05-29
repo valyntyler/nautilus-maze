aa6459cd9b6e1b79ad5a3285b456f2342e326c81
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-29 22:17:29 +0300

    feat: update playback_button.ts

--- src/ts/common/playback/playback_button.ts
+++ src/ts/common/playback/playback_button.ts
@@ -1,3 +1,5 @@
+import PlaybackEvent from "./playback_event";
+
 type PlaybackButton =
   | PlaybackButton.Prev
   | PlaybackButton.Next
@@ -7,12 +9,41 @@ type PlaybackButton =
   | PlaybackButton.Reset;
 
 namespace PlaybackButton {
-  export type Prev = { name: "prev"; source: "./assets/bx-skip-previous.svg" };
-  export type Next = { name: "next"; source: "./assets/bx-skip-next.svg" };
-  export type Play = { name: "play"; source: "./assets/bx-play.svg" };
-  export type Stop = { name: "stop"; source: "./assets/bx-stop.svg" };
-  export type Pause = { name: "pause"; source: "./assets/bx-pause.svg" };
-  export type Reset = { name: "reset"; source: "./assets/bx-rotate-ccw.svg" };
+  export type Prev = {
+    name: "prev";
+    source: "./assets/bx-skip-previous.svg";
+    event: PlaybackEvent.Prev;
+  };
+
+  export type Next = {
+    name: "next";
+    source: "./assets/bx-skip-next.svg";
+    event: PlaybackEvent.Next;
+  };
+
+  export type Play = {
+    name: "play";
+    source: "./assets/bx-play.svg";
+    event: PlaybackEvent.Play;
+  };
+
+  export type Stop = {
+    name: "stop";
+    source: "./assets/bx-stop.svg";
+    event: PlaybackEvent.Stop;
+  };
+
+  export type Pause = {
+    name: "pause";
+    source: "./assets/bx-pause.svg";
+    event: PlaybackEvent.Pause;
+  };
+
+  export type Reset = {
+    name: "reset";
+    source: "./assets/bx-rotate-ccw.svg";
+    event: PlaybackEvent.Stop;
+  };
 }
 
 export default PlaybackButton;
