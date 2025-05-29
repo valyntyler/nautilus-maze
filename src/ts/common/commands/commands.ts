9a6d3eaa33d1a2bd3c36bf3551a0b684f7f4e527
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-30 01:02:53 +0300

    feat: create commands.ts

---
+++ src/ts/common/commands/commands.ts
@@ -0,0 +1,24 @@
+import Command from "../../data/command";
+import Grid from "../../data/grid";
+import Transform from "../../data/transform";
+
+export default class Commands {
+  private container: HTMLTextAreaElement;
+  private steps: Array<Transform>;
+
+  constructor(t: Transform, grid: Grid) {
+    this.container = document.getElementById("commands") as HTMLTextAreaElement;
+    this.steps = [
+      t,
+      ...this.container.value
+        .trim()
+        .split(/\s+/)
+        .map((string) => Command.parse(string))
+        .filter((command) => command !== null)
+        .map((cmd) => {
+          t = Command.run(cmd, t, grid);
+          return t;
+        }),
+    ];
+  }
+}
