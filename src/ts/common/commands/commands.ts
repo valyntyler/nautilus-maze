5d04c6326c817da909052e68e3d6753e76ab8b85
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-05-30 03:35:53 +0300

    refactor: update commands.ts

--- src/ts/common/commands/commands.ts
+++ src/ts/common/commands/commands.ts
@@ -4,25 +4,19 @@ import Transform from "../../data/transform";
 
 export default class Commands {
   private container: HTMLDivElement;
+
   public steps: Array<Transform>;
 
-  constructor(t: Transform, grid: Grid) {
-    this.container = document.getElementById("commands") as HTMLDivElement;
+  private appendCommand(cmd: Command) {
+    const div = document.createElement("div");
+    div.className = "command";
+    div.textContent = Command.name(cmd);
 
-    // this.steps = [
-    //   t,
-    //   ...this.container.value
-    //     .trim()
-    //     .split(/\s+/)
-    //     .map((string) => Command.parse(string))
-    //     .filter((command) => command !== null)
-    //     .map((cmd) => {
-    //       t = Command.run(cmd, t, grid);
-    //       return t;
-    //     }),
-    // ];
-    //
-    // console.log(this.steps);
+    this.container.appendChild(div);
+  }
+
+  private html() {
+    this.container = document.getElementById("commands") as HTMLDivElement;
 
     const form = document.getElementById("command-prompt") as HTMLFormElement;
     const input = form.elements.namedItem("command") as HTMLInputElement;
@@ -30,18 +24,46 @@ export default class Commands {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
 
-      const div = document.createElement("div");
-      div.className = "command";
-      div.textContent = input.value;
-
-      if (
-        Command.values()
-          .map((cmd) => Command.name(cmd))
-          .includes(input.value)
-      ) {
-        this.container.appendChild(div);
+      const cmd = Command.parse(input.value);
+      if (cmd !== null) {
+        this.appendCommand(cmd);
         input.value = "";
+
+        localStorage.setItem(
+          "commands",
+          JSON.stringify(
+            Array.from(this.container.children).map((elem) =>
+              Command.parse(elem.textContent!),
+            ),
+          ),
+        );
       }
     });
+  }
+
+  private revive() {
+    const local = localStorage.getItem("commands");
+    if (local) {
+      (JSON.parse(local) as Array<Command>).forEach((cmd) =>
+        this.appendCommand(cmd),
+      );
+    }
+  }
+
+  constructor(transform: Transform, grid: Grid) {
+    this.html();
+    this.revive();
+
+    let current = transform;
+    this.steps = [
+      transform,
+      ...Array.from(this.container.children)
+        .map((elem) => Command.parse(elem.textContent!))
+        .filter((command) => command !== null)
+        .map((cmd) => {
+          current = Command.run(cmd, current, grid);
+          return current;
+        }),
+    ];
   }
 }
