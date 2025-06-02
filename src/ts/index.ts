9dca020081cdbc275e42376d7e69c8c4c3ea5612
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-02 19:41:38 +0300

    fix: remembering active window

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,11 +4,19 @@ import PuzzleRunner from "./common/puzzle_runner";
 import PuzzleEditor from "./common/puzzle_editor";
 
 document.addEventListener("DOMContentLoaded", () => {
-  let puzzle: Puzzle = new PuzzleRunner();
+  let local = localStorage.getItem("active-window");
+  let puzzle: Puzzle;
+
+  if (local !== null && local === "edit") {
+    puzzle = new PuzzleEditor();
+  } else {
+    puzzle = new PuzzleRunner();
+  }
 
   const onview = () => {
     // window.history.pushState({}, "", "/view");
     puzzle = new PuzzleRunner();
+    localStorage.setItem("active-window", "view");
 
     puzzle.onview = onview;
     puzzle.onedit = onedit;
@@ -17,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const onedit = () => {
     // window.history.pushState({}, "", "/edit");
     puzzle = new PuzzleEditor();
+    localStorage.setItem("active-window", "edit");
 
     puzzle.onview = onview;
     puzzle.onedit = onedit;
