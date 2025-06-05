e34ee958efa86c54613e964fea3027c5028f69c8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-05 14:57:36 +0300

    feat: serving on argument-provided port

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	port := 8080
+	port := flag.Uint("port", 8080, "The serial port on which to run the server")
+	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir("./src/public")))
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 
-	fmt.Printf("Server open at http://localhost:%d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	fmt.Printf("Server open at http://localhost:%d\n", *port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
