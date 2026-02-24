3ec2a47337ceb3089ca9938fa1e6943a19e2ce17
Valyn Tyler <git@valyntyler.com>
2026-02-24 21:57:48 +0100

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [nodejs];
+        };
+      };
+    };
+}
