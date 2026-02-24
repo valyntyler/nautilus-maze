b5af4b9a1914823b677796f75624a4fcaca7dfea
Valyn Tyler <git@valyntyler.com>
2026-02-24 20:29:24 +0100

    Revert "fix: go server"
    This reverts commit f9d1d44161b375f860e2a89a03501b0db59b7910.

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -5,27 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 func main() {
 	port := flag.Uint("port", 8080, "The serial port on which to run the server")
 	flag.Parse()
 
-	dist := "./dist"
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := filepath.Join(dist, r.URL.Path)
-
-		if _, err := os.Stat(path); os.IsNotExist(err) {
-			http.ServeFile(w, r, filepath.Join(dist, "index.html"))
-			return
-		}
-
-		http.FileServer(http.Dir(dist)).ServeHTTP(w, r)
-	})
+	http.Handle("/", http.FileServer(http.Dir("./src/public")))
+	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 
 	fmt.Printf("Server open at http://localhost:%d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
