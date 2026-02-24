586678bebf2023b6be987722edfc612145edcd22
Valyn Tyler <git@valyntyler.com>
2026-02-24 21:40:48 +0100

    fix: file serving

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -11,8 +11,7 @@ func main() {
 	port := flag.Uint("port", 8080, "The serial port on which to run the server")
 	flag.Parse()
 
-	http.Handle("/", http.FileServer(http.Dir("./src/public")))
-	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
+	http.Handle("/", http.FileServer(http.Dir("./out")))
 
 	fmt.Printf("Server open at http://localhost:%d\n", *port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
